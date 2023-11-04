e5711b196f3e53d361a31f5500ff9907e3d84909
Aniket Kumar <kraniket123654@gmail.com>
2023-11-04 17:37:25 +0530

    few changes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,29 @@
+import { useEffect, useState } from "react";
 import SEO from "../common/seo";
 import Home from "../components/homes/home";
 import Wrapper from "../layout/wrapper";
 
 
 const index = () => {
+  const [showImage, setShowImage] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShowImage(false);
+    }, 5000);
+    return () => clearTimeout(timeout);
+  }, []);
+
+
   return (
     <Wrapper>
       <SEO pageTitle={'Xchange'} />
+      <div style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
+
+      {showImage && (
+        <img width="95%" src="/assets/img/hb.png" alt="Your Image" />
+      )}
+      </div>
       <Home />
     </Wrapper>
   );
