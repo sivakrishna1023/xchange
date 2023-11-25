ddab6ecf3ab8e81bda41eec2e53b04ecd2be23e4
Siva Krishna <chukkakrishna999@gmail.com>
2023-11-25 19:01:05 +0530

    with out varification

--- src/pages/api/ads/Newads.js
+++ src/pages/api/ads/Newads.js
@@ -31,8 +31,8 @@ const handler = asyncError(async (req, res) => {
    console.log(draft);
   const user = await checkAuth(req);
   if (!user) return errorHandler(res, 401, "Login First");
-   const verified=user.isverified;
-  if(!verified) return errorHandler(res,400,"Please varify Your mail to continue..!!");
+  const verified=user.isverified;
+  // if(!verified) return errorHandler(res,400,"Please varify Your mail to continue..!!");
       const newad =await Ads.create({
         Category,
         Adname,
