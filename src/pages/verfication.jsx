e8ae380a3028efbd2c125ddb6cd9c859d278c99f
Siva Krishna <chukkakrishna999@gmail.com>
2023-12-28 19:19:46 +0530

    With error in verification page

--- src/pages/verfication.jsx
+++ src/pages/verfication.jsx
@@ -10,9 +10,6 @@ const verfication = () => {
   const [loading,setloading]=useState(false);
   const [verified,setverified]=useState(false);
   const router = useRouter();
-  setTimeout(() => {
-    console.log("This will be executed after 3 seconds.");
-  }, 100);
   const get_me_verified=async()=>{
     const queryParams = router.query;
     const token = queryParams.token;
@@ -31,41 +28,39 @@ const verfication = () => {
          })
          const data=await res.json();
          if(data.success){
-              setverified(data.verified);
+              setloading(false);
+              setverified(true);
          }
        }catch(error){
+        setloading(false);
        }
     setloading(false);
   }
   useEffect(() => {
-    get_me_verified();
+     get_me_verified();
   }, []);
-  if(loading===true){
-    return (
-    <Wrapper>
-    <SEO pageTitle={"verfication"} />
-    <div style={{display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center", height:"40vh"}}>
-    <h2>Please Wait...!!</h2>
-    </div>
-    </Wrapper>
-    )
-  }
+  // if(loading===true){
+  //   return (
+  //   <Wrapper>
+  //   <SEO pageTitle={"verfication"} />
+  //   <div style={{display:"flex",paddingTop:"20vh", flexDirection:"column", justifyContent:"center", alignItems:"center", height:"40vh"}}> <br /> <br /> <br />
+  //   <h4>Please Wait...!!</h4>
+  //   </div>
+  //   </Wrapper>
+  //   )
+  // }
   return (
     <Wrapper>
     <SEO pageTitle={"verfication"} />
     <Breadcrumb title="Verification" subtitle="Verification" isDbbl="Pages" />
-    <div style={{display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center", height:"40vh"}}>
-    {
-      !verified && !loading ? ( <div><h2>Failed to Verify</h2>
-      <h1>  <button onClick={get_me_verified()}> Click here </button> to try again</h1> </div> ) :(
-         <div></div>
+    <div style={{display:"flex",paddingTop:"20vh", flexDirection:"column", justifyContent:"center", alignItems:"center", height:"40vh"}}>
+      {
+      !verified ? ( <div><h2>Failed to Verify</h2>
+      <h1>  <button onClick={get_me_verified()}> Click here </button> to try again</h1> </div> ) 
+      :(
+        <Link href="/post-ad"><div style={{ padding: "10px", margin: "1px", backgroundColor: "#19ae50", borderRadius: "10px", color: "white", fontWeight: "bolder", marginTop: "1rem", cursor: "pointer", display: "flex", justifyContent: "center", alignItems: "center" }}>Continue posting ads</div></Link>
       )
-    }
-    {
-      verified ? (
-        <Link href="/"><div style={{ padding: "10px", margin: "1px", backgroundColor: "#19ae50", borderRadius: "10px", color: "white", fontWeight: "bolder", marginTop: "1rem", cursor: "pointer", display: "flex", justifyContent: "center", alignItems: "center" }}>Continue posting ads</div></Link>
-      ) : (<div> </div> )
-    }
+      }
     </div>
     </Wrapper>
   )
