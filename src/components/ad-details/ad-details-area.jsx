404eff382d8ef1423fca215e99fff709421b31e0
Siva Krishna <chukkakrishna999@gmail.com>
2024-01-02 09:25:33 +0530

    Error in review

--- src/components/ad-details/ad-details-area.jsx
+++ src/components/ad-details/ad-details-area.jsx
@@ -213,7 +213,7 @@ const AdDetailsArea = () => {
                                        <input style={{ width: "100%", marginBottom: "1rem", padding: "1rem" }} type="text" placeholder='Write Review Here' />
                                        <Popover placement="bottom">
                                        <PopoverTrigger>
-                                       <Link className="tp-vp-btn-green">Review</Link>
+                                       <div className="tp-vp-btn-green">Review</div>
                                        </PopoverTrigger>
                                        <PopoverContent>
                                           <div className="px-1 py-2">
