0dda1b3e3f67d8a1f20637a314fa70f431cc77a0
Siva Krishna <chukkakrishna999@gmail.com>
2024-01-16 00:59:17 +0530

    change_1 in sitemap

--- src/components/sitemap/index.jsx
+++ src/components/sitemap/index.jsx
@@ -4,17 +4,17 @@ import React from 'react';
 const Sitemap = () => {
   return (
     `<?xml version="1.0" encoding="UTF-8"?>
-     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-         <url>
-             <loc>https://www.xchangehyd.com/</loc>
-         </url>
-         <url>
-             <loc>https://www.xchangehyd.com/sign-in</loc>
-         </url>
-         <url>
-             <loc>https://www.xchangehyd.com/register</loc>
-         </url>
-     </urlset>`
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        <url>
+            <loc>https://www.xchangehyd.com/</loc>
+        </url>
+        <url>
+            <loc>https://www.xchangehyd.com/sign-in</loc>
+        </url>
+        <url>
+            <loc>https://www.xchangehyd.com/register</loc>
+        </url>
+    </urlset>`
   );
 };
 
