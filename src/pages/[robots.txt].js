a73f2ebd7e6b3354277641596b734296dbffeba8
Siva Krishna <chukkakrishna999@gmail.com>
2024-01-17 23:36:59 +0530

    changes in robots.txt file

--- src/pages/[robots.txt].js
+++ src/pages/[robots.txt].js
@@ -1,15 +1,6 @@
-// pages/[slug].js
-
-import { useRouter } from 'next/router';
-
 const RobotsTxt = () => {
-  const router = useRouter();
-  const { slug } = router.query;
-
-  // Customize the response based on the slug
   const content = `User-agent: *\nDisallow:`;
-
-  return <pre>{content}</pre>;
+  return <>{content}</>;
 };
 
 export default RobotsTxt;
