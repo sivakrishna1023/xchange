0bfa84f48b4ebb3236f914238e13b410c1926008
Siva Krishna <chukkakrishna999@gmail.com>
2024-01-17 22:49:03 +0530

    indexing allowed

--- src/common/seo.jsx
+++ src/common/seo.jsx
@@ -10,7 +10,7 @@ const SEO = ({ pageTitle }) => (
       </title>
       <meta httpEquiv="x-ua-compatible" content="ie=edge" />
       <meta name="description" content="Your Trusted Online Market Place" />
-      <meta name="robots" content="noindex, follow" />
+      <meta name="robots" content="index, follow" />
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, shrink-to-fit=no"
