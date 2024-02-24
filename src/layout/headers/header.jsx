89ef9fb649b7e7fabbb68a9a113d5030176f310e
Aniket Kumar <kraniket123654@gmail.com>
2024-02-24 16:21:06 +0530

    yo

--- src/layout/headers/header.jsx
+++ src/layout/headers/header.jsx
@@ -7,8 +7,25 @@ import NavMenu from "./nav-menu";
 import Sidebar from "./sidebar";
 import { Context } from "@/src/components/Clients/clientcomponents";
 import NavMenu2 from "./nav-menu2";
+import Popup from "reactjs-popup";
+import 'reactjs-popup/dist/index.css';
+import { IoIosCloseCircle } from "react-icons/io";
 
 const Header = () => {
+  const cities = [
+    { id: 1, name: 'Delhi' },
+    { id: 2, name: 'Mumbai' },
+    { id: 3, name: 'Kolkata' },
+    // Add more cities as needed
+  ];
+
+  // State to store the selected city
+  const [selectedCity, setSelectedCity] = useState('');
+
+  // Event handler for changing the selected city
+  const handleCityChange = (event) => {
+    setSelectedCity(event.target.value);
+  };
   const [isMobile, setIsMobile] = useState(false);
   const [searchText, setSearchText] = useState("");
   const router = useRouter();
@@ -16,7 +33,7 @@ const Header = () => {
   const isAdListRoute = router.pathname === '/ad-list';
 
   const handleSearch = () => {
-    if(searchText!==''){
+    if (searchText !== '') {
       window.location.href = `/ad-list?select=${searchText}`;
     }
   }
@@ -34,7 +51,7 @@ const Header = () => {
   const [isActive, setIsActive] = useState(false);
   const { user } = useContext(Context);
   const token = typeof window !== 'undefined' ? localStorage.getItem('token') : null;
-  
+
   return (
     <>
       <header className="header__transparent ">
@@ -75,12 +92,12 @@ const Header = () => {
                 <div className="col-xxl-9 col-xl-9 col-lg-7 col-md-6 col-6 d-flex align-items-center justify-content-end">
                   {(isMobile || isAdListRoute) ? (
                     <div >
-                      
+
                     </div>
                   ) : (
-                    <div style={{marginRight:"1rem", position:"relative"}}>
-                      <i className="fa fa-search" style={{position:"absolute", right:"20px", top:"8px", color:"#5A5A5A", fontSize:"20px", cursor:"pointer"}} onClick={handleSearch}></i>
-                      <input style={{ width:"300px", borderRadius: "15px", border: "none", padding: "7px 20px", marginRight: "0.5rem" }} type="text" placeholder="Search..." onChange={(e)=>{setSearchText(e.target.value)}} />
+                    <div style={{ marginRight: "1rem", position: "relative" }}>
+                      <i className="fa fa-search" style={{ position: "absolute", right: "20px", top: "8px", color: "#5A5A5A", fontSize: "20px", cursor: "pointer" }} onClick={handleSearch}></i>
+                      <input style={{ width: "300px", borderRadius: "15px", border: "none", padding: "7px 20px", marginRight: "0.5rem" }} type="text" placeholder="Search..." onChange={(e) => { setSearchText(e.target.value) }} />
                       {console.log(searchText)}
                     </div>
                   )}
@@ -88,17 +105,35 @@ const Header = () => {
                     <nav id="mobile-menu" className="d-none d-xl-block">
                       <NavMenu />
                     </nav>
-                    <Link href={user._id?"/post-ad":"/sign-in"} style={{padding:"15px", margin:"10px 0", fontFamily:"sans-serif", backgroundColor:"rgba(255, 255, 255, 0.1)", display:"flex", justifyContent:"center", alignItems:"center", border:"1px solid white"}} className="tp-btn">SELL</Link>
+                    <Link style={{ padding: "15px", margin: "10px 0", fontFamily: "sans-serif", backgroundColor: "rgba(255, 255, 255, 0.1)", display: "flex", justifyContent: "center", alignItems: "center", border: "1px solid white" }} className="tp-btn">SELL</Link>
+                    <Popup trigger={<Link href={user._id ? "/post-ad" : "/sign-in"} style={{ padding: "15px", margin: "10px 0 10px 10px", fontFamily: "sans-serif", backgroundColor: "rgba(255, 255, 255, 0.1)", display: "flex", justifyContent: "center", alignItems: "center", border: "1px solid white" }} className="tp-btn">Location</Link>} modal>
+        {close => (<div style={{ height: '200px', display: 'flex', justifyContent: 'center', alignItems: 'center', position: 'relative', borderRadius: '20px' }}>
+          <button onClick={close} style={{ position: 'absolute', top: '0', right: '0' }}><IoIosCloseCircle size={30} /></button>
+          <button style={{ position: 'absolute', bottom: '0', right: '0', padding: '10px 15px', margin: '1rem', borderRadius: '10px', backgroundColor: '#19ae51', color: 'white', fontWeight: '600' }}>Confirm</button>
+          <label style={{ fontSize: '20px' }}>
+            Choose your location
+            <select value={selectedCity} onChange={handleCityChange} style={{ backgroundColor: 'white', padding: '10px 15px', margin: '1rem', borderRadius: '10px' }}>
+              <option value="">Select a city</option>
+              {cities.map((city) => (
+                <option key={city.id} value={city.name}>
+                  {city.name}
+                </option>
+              ))}
+            </select>
+          </label>
+        </div>
+        )}
+      </Popup>
                   </div>
                   <div className="header-right d-md-flex align-items-center">
                     <div className="header-meta">
                       <ul>
-                        <div style={{display:"flex"}}>
-                        <li style={{margin:"0"}}>
-                          <a onClick={() => setIsActive(true)} href="#" className="tp-menu-toggle d-xl-none">
-                            <i className="icon_ul"></i>
-                          </a>
-                        </li>
+                        <div style={{ display: "flex" }}>
+                          <li style={{ margin: "0" }}>
+                            <a onClick={() => setIsActive(true)} href="#" className="tp-menu-toggle d-xl-none">
+                              <i className="icon_ul"></i>
+                            </a>
+                          </li>
                         </div>
 
                       </ul>
